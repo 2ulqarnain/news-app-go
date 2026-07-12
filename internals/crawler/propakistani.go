141059baf40a4a422ae38206961a4624ec04ee9e
2ulqarnain <zulqernaynh@gmail.com>
2026-07-12 13:59:21 +0500

    blocked more urls for faster browser, added html ss for debugging, also added inserted log

--- internals/crawler/propakistani.go
+++ internals/crawler/propakistani.go
@@ -2,60 +2,93 @@ package crawler
 
 import (
 	"GoNewsScrapper/internals/database"
-	"GoNewsScrapper/internals/repository"
 	"context"
+	"log"
 	"time"
 
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 )
 
-type Crawler struct {
-	repo *repository.News
-}
-
-func New(repo *repository.News) *Crawler {
-	return &Crawler{repo: repo}
-}
-
-func (c *Crawler) CrawlProPakistaniPk() error {
-
+func CrawlProPakistaniPk(ctx context.Context, timeout time.Duration) ([]database.CreateNewsParams, error) {
 	url := "https://propakistani.pk"
 
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.DisableGPU,
+		//chromedp.ExecPath("/usr/bin/chromium-browser"),
+
 		chromedp.Headless,
+		chromedp.DisableGPU,
+		chromedp.NoSandbox,
+		chromedp.NoFirstRun,
+		chromedp.NoDefaultBrowserCheck,
+
+		chromedp.Flag("disable-dev-shm-usage", true),
+		chromedp.Flag("disable-background-networking", true),
+		chromedp.Flag("disable-background-timer-throttling", true),
+		chromedp.Flag("disable-renderer-backgrounding", true),
 	)
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	allocCtx, cancel := chromedp.NewExecAllocator(ctx, opts...)
 	defer cancel()
-	ctx, cancel := chromedp.NewContext(allocCtx)
+	ctxNew, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
-	ctxWithTimeout, cancel := context.WithTimeout(ctx, 10*time.Second)
+	ctxWithTimeout, cancel := context.WithTimeout(ctxNew, timeout*time.Second)
 	defer cancel()
 
-	var pageTitle string
 	var news []database.CreateNewsParams
 
+	log.Println("crawling started...")
 	err := chromedp.Run(ctxWithTimeout,
+		network.Enable(),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			return network.SetBlockedURLs().WithURLPatterns([]*network.BlockPattern{
+				{URLPattern: "*://*/*.jpg"},
+				{URLPattern: "*://*/*.png"},
+				{URLPattern: "*://*/*.gif"},
+				{URLPattern: "*://*/*.avif"},
+				{URLPattern: "*://*/*.webp"},
+				{URLPattern: "*://*/*.svg"},
+				{URLPattern: "*://*/*.woff"},
+				{URLPattern: "*://*/*.woff2"},
+				{URLPattern: "*://*/*.ttf"},
+				{URLPattern: "*://*/*.otf"},
+				{URLPattern: "*://*/*.css"},
+				{URLPattern: "*://*doubleclick/*"},
+				{URLPattern: "*://*.youtube.*/*"},
+				{URLPattern: "*://*googletagmanager*/*"},
+			}).Do(ctx)
+		}),
 		chromedp.Navigate(url),
-		chromedp.WaitVisible("div.team-news"),
-		chromedp.Title(&pageTitle),
+		chromedp.WaitVisible("div.teams-news"),
+		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
+		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
+		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
 		chromedp.Evaluate(`
-			Array.from(document.querySelectorAll('div.tnews-inner.relv')).map(el => {
-    		let news_url = el.querySelector('a').href
-    		let image_url = el.querySelector('img').src
-    		let title = el.querySelector('h5').textContent
-    		return {news_url, image_url, title}
-		})`, &news),
+			Array.from(document.querySelectorAll(".g1-mosaic-item:has(.g1-frame img), div.tnews-inner.relv")).map(el=>{
+				let news = {}
+				news.newsUrl = el.querySelector("a").href
+				news.imageUrl = el.querySelector("img").src
+				news.title = el.querySelector("h3, h5")?.textContent
+				news.slug = news.newsUrl?.split('/')?.at(-2)
+				return news
+			})
+		`, &news),
 	)
 	if err != nil {
-		return err
+		var html string
+
+		if e := chromedp.Run(ctx,
+			chromedp.OuterHTML("html", &html),
+		); e == nil {
+			log.Println(html)
+		}
+
+		return nil, err
 	}
 
-	err = c.repo.InsertNewsBulk(ctx, news)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	log.Printf("crawling finished, crawled %d news", len(news))
+	return news, nil
 }
--- internals/service/news.go
+++ internals/service/news.go
@@ -1,15 +1,12 @@
 package service
 
 import (
+	"GoNewsScrapper/internals/crawler"
 	"GoNewsScrapper/internals/database"
 	"GoNewsScrapper/internals/repository"
 	"GoNewsScrapper/internals/utils"
 	"context"
 	"log"
-	"time"
-
-	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/chromedp"
 )
 
 type News struct {
@@ -29,74 +26,18 @@ func (n *News) GetAllNews(ctx context.Context) ([]database.News, error) {
 }
 
 func (n *News) CrawlNews() error {
-	url := "https://propakistani.pk"
-
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.ExecPath("/usr/bin/chromium-browser"),
-
-		chromedp.Headless,
-		chromedp.DisableGPU,
-		chromedp.NoSandbox,
-		chromedp.NoFirstRun,
-		chromedp.NoDefaultBrowserCheck,
-
-		chromedp.Flag("disable-dev-shm-usage", true),
-		chromedp.Flag("disable-background-networking", true),
-		chromedp.Flag("disable-background-timer-throttling", true),
-		chromedp.Flag("disable-renderer-backgrounding", true),
-	)
-
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
-	ctxWithTimeout, cancel := context.WithTimeout(ctx, 60*time.Second)
-	defer cancel()
-
-	var news []database.CreateNewsParams
-
-	log.Println("crawling started...")
-	err := chromedp.Run(ctxWithTimeout,
-		network.Enable(),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			return network.SetBlockedURLs().WithURLPatterns([]*network.BlockPattern{
-				{URLPattern: "*://*/*.jpg"},
-				{URLPattern: "*://*/*.png"},
-				{URLPattern: "*://*/*.gif"},
-				{URLPattern: "*://*/*.avif"},
-			}).Do(ctx)
-		}),
-		chromedp.Navigate(url),
-		chromedp.WaitVisible("div.teams-news"),
-		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
-		chromedp.Sleep(2*time.Second),
-		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
-		chromedp.Sleep(2*time.Second),
-		chromedp.Click("input#pp-load-posts", chromedp.ByQuery),
-		chromedp.Sleep(2*time.Second),
-		chromedp.Evaluate(`
-			Array.from(document.querySelectorAll(".g1-mosaic-item:has(.g1-frame img), div.tnews-inner.relv")).map(el=>{
-				let news = {}
-				news.newsUrl = el.querySelector("a").href
-				news.imageUrl = el.querySelector("img").src
-				news.title = el.querySelector("h3, h5")?.textContent
-				news.slug = news.newsUrl?.split('/')?.at(-2)
-				return news
-			})
-		`, &news),
-	)
+	ctx := context.Background()
+	news, err := crawler.CrawlProPakistaniPk(ctx, 30)
 	if err != nil {
-
 		return err
 	}
-
 	news = utils.RemoveNewsDuplicatesBySlug(news)
 
 	err = n.CreateNews(ctx, news)
 	if err != nil {
 		return err
 	}
+	log.Println("data insertion complete")
 
-	log.Printf("crawling finished, crawled %d news", len(news))
 	return nil
 }
